2c4316fb23265a7eb4fbcc92980f77582b3c48b8
ArinaBogan <arina.bogan@gmail.com>
2024-01-02 20:33:09 +0300

    added education_platform

--- src/helper/buildResponse.ts
+++ src/helper/buildResponse.ts
@@ -2,7 +2,8 @@ import { Response } from 'express';
 import { iCourse, iUser } from '../interfaces/interfaces';
 
 function buildResponse(res: Response, code: number, message: iCourse[] |iUser[]| string) {
-  res.status(code).send(message);
+  res.status(code);
+  res.send(message);
 }
 
 export { buildResponse };
