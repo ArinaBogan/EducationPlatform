0de7abb941d5c7c82834752ff324594cfcd59711
ArinaBogan <arina.bogan@gmail.com>
2024-01-06 21:47:29 +0300

    added education_platform

--- src/tests/repository/courses.repository.test.ts
+++ src/tests/repository/courses.repository.test.ts
@@ -1,53 +1,76 @@
-import exp from "constants";
-import { getAllCourseDB,getCourseByIdDB, postCourseDB} from "../../repository/courses.repository";
+import { getAllCourseDB, getCourseByIdDB, postCourseDB, updateCourseDB, deleteCourseByIdDB } from '../../repository/courses.repository';
 
 const mClient = {
-    query: jest.fn(),
+  query: jest.fn(),
 };
 
 jest.mock('pg', () => {
-    const pool = { connect: jest.fn(() => mClient) };
-    return { Pool: jest.fn(() => pool) }
+  const pool = { connect: jest.fn(() => mClient) };
+  return { Pool: jest.fn(() => pool) };
 });
 
 afterEach(() => {
-    jest.clearAllMocks();
+  jest.clearAllMocks();
 });
 
 describe('test getAllCoursesDB', () => {
-    test('return success', async () => {
-        mClient.query.mockResolvedValue({ rows: [{ id: 1, course: 'TS' }] });
-        const result = await getAllCourseDB();
-        expect(mClient.query).toHaveBeenCalled();
-        expect(result).toEqual([{ id: 1, course: 'TS' }]);
-        expect(result.length).toBe(1);
-        expect(result[0].id).toBe(1);
-        expect(result[0].course).toBe('TS')
-    });
+  test('return success', async () => {
+    mClient.query.mockResolvedValue({ rows: [{ id: 1, course: 'TS' }] });
+    const result = await getAllCourseDB();
+    expect(mClient.query).toHaveBeenCalled();
+    expect(result).toEqual([{ id: 1, course: 'TS' }]);
+    expect(result.length).toBe(1);
+    expect(result[0].id).toBe(1);
+    expect(result[0].course).toBe('TS');
+  });
 });
 
-describe('test getCourseByIdDB',()=>{
-    test('return success',async ()=>{
-        mClient.query.mockResolvedValue({ rows: [{ id: 1, course: 'TS' }] });
-        const result=await getCourseByIdDB(1);
+describe('test getCourseByIdDB', () => {
+  test('return success', async () => {
+    mClient.query.mockResolvedValue({ rows: [{ id: 1, course: 'TS' }] });
+    const result = await getCourseByIdDB(1);
 
-        expect(mClient.query).toHaveBeenCalled();
-        expect(result).toEqual([{ id: 1, course: 'TS' }]);
-        expect(result).toHaveLength(1);
-        expect(result[0].id).toBe(1);
-        expect(result[0].course).toBe('TS');
-    });
+    expect(mClient.query).toHaveBeenCalled();
+    expect(result).toEqual([{ id: 1, course: 'TS' }]);
+    expect(result).toHaveLength(1);
+    expect(result[0].id).toBe(1);
+    expect(result[0].course).toBe('TS');
+  });
 });
 
-describe('test postCourseDB',()=>{
-    test('return success',async()=>{
-        mClient.query.mockResolvedValue({ rows: [{ id: 1, course: 'TS' }] });
-        const result=await postCourseDB('TS');
+describe('test postCourseDB', () => {
+  test('return success', async () => {
+    mClient.query.mockResolvedValue({ rows: [{ id: 1, course: 'TS' }] });
+    const result = await postCourseDB('TS');
 
-        expect(mClient.query).toHaveBeenCalled();
-        expect(result).toEqual([{ id: 1, course: 'TS' }]);
-        expect(result).toHaveLength(1);
-        expect(result[0].id).toBe(1);
-        expect(result[0].course).toBe('TS');
-    });
+    expect(mClient.query).toHaveBeenCalled();
+    expect(result).toEqual([{ id: 1, course: 'TS' }]);
+    expect(result).toHaveLength(1);
+    expect(result[0].id).toBe(1);
+    expect(result[0].course).toBe('TS');
+  });
+});
+
+describe('test updateCourseDB', () => {
+  test('return success', async () => {
+    mClient.query.mockResolvedValue({ rows: [{ id: 1, course: 'TS' }] });
+    const result = await updateCourseDB(1, 'TS');
+
+    expect(mClient.query).toHaveBeenCalled();
+    expect(result).toEqual([{ id: 1, course: 'TS' }]);
+    expect(result).toHaveLength(1);
+    expect(result[0].id).toBe(1);
+    expect(result[0].course).toBe('TS');
+  });
+});
+
+describe('test deleteCourseByIdDB', () => {
+  test('return success', async () => {
+    mClient.query.mockResolvedValue({ rows: [{ id: 1 }] });
+    const result = await deleteCourseByIdDB(1);
+
+    expect(mClient.query).toHaveBeenCalled();
+    expect(result).toEqual([{ id: 1 }]);
+    expect(result).toHaveLength(1);
+  });
 });
--- src/tests/service/api.service.test.ts
+++ src/tests/service/api.service.test.ts
@@ -4,116 +4,118 @@ import { registrationUser, authorizationUser } from '../../service/api.service';
 import bcrypt from 'bcrypt';
 
 describe('registrationUser test', () => {
-    test('return success', async () => {
-        const mockCheckEmail = jest.spyOn(repository, 'getUserByEmailDB');
-        mockCheckEmail.mockResolvedValue([]);
+  test('return success', async () => {
+    const mockCheckEmail = jest.spyOn(repository, 'getUserByEmailDB');
+    mockCheckEmail.mockResolvedValue([]);
 
-        const bcryptHash = jest.spyOn(bcrypt, 'hash');
-        bcryptHash.mockResolvedValue('123')
-        const mockRegistration = jest.spyOn(repository, 'registrationUserDB');
-        mockRegistration.mockResolvedValue([{
-            id: 1,
-            name: 'Test1',
-            surname: 'Test1',
-            email: 'Test@gmail.com',
-            pwd: '123456789',
-        },])
-        const result = await registrationUser('Test1', 'Test1', 'Test@gmail.com', '123456789');
-        expect(mockCheckEmail).toHaveBeenCalled();
-        expect(mockRegistration).toHaveBeenCalled();
-        expect(result[0].name).toBe('Test1');
-        expect(result).toEqual([
-            { id: 1, name: 'Test1', surname: 'Test1', email: 'Test@gmail.com', pwd: '123456789' },]
-        );
+    const bcryptHash = jest.spyOn(bcrypt, 'hash');
+    bcryptHash.mockResolvedValue('123');
+    const mockRegistration = jest.spyOn(repository, 'registrationUserDB');
+    mockRegistration.mockResolvedValue([
+      {
+        id: 1,
+        name: 'Test1',
+        surname: 'Test1',
+        email: 'Test@gmail.com',
+        pwd: '123456789',
+      },
+    ]);
+    const result = await registrationUser('Test1', 'Test1', 'Test@gmail.com', '123456789');
+    expect(mockCheckEmail).toHaveBeenCalled();
+    expect(mockRegistration).toHaveBeenCalled();
+    expect(result[0].name).toBe('Test1');
+    expect(result).toEqual([{ id: 1, name: 'Test1', surname: 'Test1', email: 'Test@gmail.com', pwd: '123456789' }]);
 
-        expect(bcryptHash).toHaveBeenCalled();
-    });
-    test('return error checkEmail', async () => {
-        const mockCheckEmail = jest.spyOn(repository, 'getUserByEmailDB');
-        mockCheckEmail.mockResolvedValue([{
-            id: 1,
-            name: 'Test1',
-            surname: 'Test1',
-            email: 'Test@gmail.com',
-            pwd: '123456789',
-        },]);
-        try {
-            await registrationUser('Test1', 'Test1', 'Test@gmail.com', '123456789');
-        } catch (error: any) {
-            expect(error.message).toBe(ExceptionType.API_CHECK_EMAIL);
-        }
-    });
-    test('return error registration', async () => {
-        const mockRegistration = jest.spyOn(repository, 'registrationUserDB');
-        mockRegistration.mockResolvedValue([]);
-        try {
-            await registrationUser('Test1', 'Test1', 'Test@gmail.com', '123456789');
-        } catch (error: any) {
-            expect(error.message).toBe(ExceptionType.API_CHECK_EMAIL);
-        }
-    });
+    expect(bcryptHash).toHaveBeenCalled();
+  });
+  test('return error checkEmail', async () => {
+    const mockCheckEmail = jest.spyOn(repository, 'getUserByEmailDB');
+    mockCheckEmail.mockResolvedValue([
+      {
+        id: 1,
+        name: 'Test1',
+        surname: 'Test1',
+        email: 'Test@gmail.com',
+        pwd: '123456789',
+      },
+    ]);
+    try {
+      await registrationUser('Test1', 'Test1', 'Test@gmail.com', '123456789');
+    } catch (error: any) {
+      expect(error.message).toBe(ExceptionType.API_CHECK_EMAIL);
+    }
+  });
+  test('return error registration', async () => {
+    const mockRegistration = jest.spyOn(repository, 'registrationUserDB');
+    mockRegistration.mockResolvedValue([]);
+    try {
+      await registrationUser('Test1', 'Test1', 'Test@gmail.com', '123456789');
+    } catch (error: any) {
+      expect(error.message).toBe(ExceptionType.API_CHECK_EMAIL);
+    }
+  });
 });
 
 describe('authorizationUser', () => {
-    test('return success', async () => {
-        const mockCheckEmail = jest.spyOn(repository, 'getUserByEmailDB');
-        mockCheckEmail.mockResolvedValue([
-            {
-                id: 1,
-                name: 'Test1',
-                surname: 'Test1',
-                email: 'Test@gmail.com',
-                pwd: '123456789',
-            }
-        ]);
+  test('return success', async () => {
+    const mockCheckEmail = jest.spyOn(repository, 'getUserByEmailDB');
+    mockCheckEmail.mockResolvedValue([
+      {
+        id: 1,
+        name: 'Test1',
+        surname: 'Test1',
+        email: 'Test@gmail.com',
+        pwd: '123456789',
+      },
+    ]);
 
-        const bcryptHash = jest.spyOn(bcrypt, 'compare');
-        bcryptHash.mockResolvedValue(true);
+    const bcryptHash = jest.spyOn(bcrypt, 'compare');
+    bcryptHash.mockResolvedValue(true);
 
-        const result = await authorizationUser('Test@gmail.com', '123456789');
-        expect(mockCheckEmail).toHaveBeenCalled();
-        expect(bcryptHash).toHaveBeenCalled();
-        expect(result[0].id).toBe(1);
-        expect(result).toHaveLength(1);
-        expect(result[0].name).toBe('Test1');
-        expect(result).toEqual([
-            {
-                id: 1,
-                name: 'Test1',
-                surname: 'Test1',
-                email: 'Test@gmail.com',
-                pwd: '123456789',
-            },
-        ]);
-    });
+    const result = await authorizationUser('Test@gmail.com', '123456789');
+    expect(mockCheckEmail).toHaveBeenCalled();
+    expect(bcryptHash).toHaveBeenCalled();
+    expect(result[0].id).toBe(1);
+    expect(result).toHaveLength(1);
+    expect(result[0].name).toBe('Test1');
+    expect(result).toEqual([
+      {
+        id: 1,
+        name: 'Test1',
+        surname: 'Test1',
+        email: 'Test@gmail.com',
+        pwd: '123456789',
+      },
+    ]);
+  });
 
-    test('return error email', async () => {
-        const mockCheckEmail = jest.spyOn(repository, 'getUserByEmailDB');
-        mockCheckEmail.mockResolvedValue([]);
-        try {
-            await authorizationUser('Test@gmail.com', '123456789')
-        } catch (error: any) {
-            expect(error.message).toBe(ExceptionType.API_EMAIL_IS_NOT_CREATE)
-        }
-    });
+  test('return error email', async () => {
+    const mockCheckEmail = jest.spyOn(repository, 'getUserByEmailDB');
+    mockCheckEmail.mockResolvedValue([]);
+    try {
+      await authorizationUser('Test@gmail.com', '123456789');
+    } catch (error: any) {
+      expect(error.message).toBe(ExceptionType.API_EMAIL_IS_NOT_CREATE);
+    }
+  });
 
-    test('return error pwd', async () => {
-        const mockCheckEmail = jest.spyOn(repository, 'getUserByEmailDB');
-        const bcryptHash = jest.spyOn(bcrypt, 'compare');
-        bcryptHash.mockResolvedValue(false);
-        mockCheckEmail.mockResolvedValue([
-            {
-                id: 1,
-                name: 'Test1',
-                surname: 'Test1',
-                email: 'Test@gmail.com',
-                pwd: '123456789',
-            },
-        ]);
-        try {
-            await authorizationUser('Test@gmail.com', '123456789');
-        } catch (error: any) {
-            expect(error.message).toBe(ExceptionType.API_IS_NOT_MATCH_PASSWORD);
-        }
-    })
+  test('return error pwd', async () => {
+    const mockCheckEmail = jest.spyOn(repository, 'getUserByEmailDB');
+    const bcryptHash = jest.spyOn(bcrypt, 'compare');
+    bcryptHash.mockResolvedValue(false);
+    mockCheckEmail.mockResolvedValue([
+      {
+        id: 1,
+        name: 'Test1',
+        surname: 'Test1',
+        email: 'Test@gmail.com',
+        pwd: '123456789',
+      },
+    ]);
+    try {
+      await authorizationUser('Test@gmail.com', '123456789');
+    } catch (error: any) {
+      expect(error.message).toBe(ExceptionType.API_IS_NOT_MATCH_PASSWORD);
+    }
+  });
 });
