4dbba3eaa9e85957f9e7820301bcf0b8efe4a9ea
ArinaBogan <arina.bogan@gmail.com>
2024-01-07 23:40:54 +0300

    added education_platform

--- src/tests/repository/user.repository.test.ts
+++ src/tests/repository/user.repository.test.ts
@@ -40,3 +40,24 @@ describe('test getUserByIdDB', () => {
         expect(result[0].surname).toBe('Test1');
     });
 });
+
+describe('test updateUserDB', () => {
+    test('return success', async () => {
+        mClient.query.mockResolvedValue({ rows: [{ id: 1, name: 'Test1', surname: 'Test1', email: 'Test1@gmail.com', pwd: '123456789' }] });
+        const result = await updateUserDB(1, 'Test1', 'Test1', 'Test1@gmail.com', '123456789');
+
+        expect(mClient.query).toHaveBeenCalled();
+        expect(result).toEqual([{ id: 1, name: 'Test1', surname: 'Test1', email: 'Test1@gmail.com', pwd: '123456789' }]);
+        expect(result).toHaveLength(1);
+        expect(result[0].id).toBe(1);
+        expect(result[0].email).toBe('Test1@gmail.com');
+        expect(result[0].surname).toBe('Test1');
+        expect(result[0].name).toBe('Test1');
+    });
+});
+
+describe('test deleteUserDB',()=>{
+    test('return success',async()=>{
+        
+    })
+})
