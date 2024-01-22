43dbbc4a5263a8957ceb969bf98bc26a617f2ba5
ArinaBogan <arina.bogan@gmail.com>
2024-01-22 17:43:43 +0300

    added client

--- client/src/components/Preview/Preview.js
+++ client/src/components/Preview/Preview.js
@@ -13,17 +13,42 @@ function Preview() {
                         <div className={style.lightning}></div>
                         <p className={style.studentsCount}>600 <span>+</span></p>
                     </div>
-                    <p className={style.textStudents}></p>
+                    <p className={style.textStudents}>Students</p>
                 </div>
-
                 <div className={style.previewImg}></div>
             </div>
 
 
+            <div className={style.learnContent}>
+                <div className={style.learnMainImg}></div>
+                <div className={style.learnInfo}>
+                    <h2>Learn a language in a playful way</h2>
+                    <p>Make learning programming languages more fun with mini-games</p>
+                    <div className={style.learnImgs}>
+                        <div className={style.shoesImg}></div>
+                        <div className={style.speakerImg}></div>
+                    </div>
+                </div>
+            </div>
 
-            <div className={style.lernContent}></div>
-            <div className={style.knowledgeContent}></div>
-            <div className={style.progressContent}></div>
+            <div className={style.knowledgeContent}>
+                <div className={style.knowledgeInfo}>
+                    <h2>Increase your knowledge</h2>
+                    <p>Traditional and new effective approaches to learning languages</p>
+                    <div className={style.btn}>Textbook →</div>
+                </div>
+                <div className={style.knowledgeImg}></div>
+
+            </div>
+
+            <div className={style.progressContent}>
+                <div className={style.progressImg}></div>
+                <div className={style.progressInfo}>
+                    <h2>Watch your progress every day</h2>
+                    <p>Save statistics on your achievements and mistakes</p>
+                    <div className={style.btn}>Statistics →</div>
+                </div>
+            </div>
         </div>
     )
 }
