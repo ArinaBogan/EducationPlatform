cc2682360add758c4a242e3d71e8a14bfe7ce84d
ArinaBogan <arina.bogan@gmail.com>
2024-01-25 22:23:03 +0300

    added client

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -4,7 +4,7 @@ import style from './style.module.css';
 function Header() {
     return (
         <div className={style.wrapper}>
-            <h1>Hschool</h1>
+            <Link to={'/'}><h1>Hschool</h1></Link>
             <div className={style.btns}>
                 <Link to={'/log'}><div className={style.login}>Login →</div></Link>
                 <Link to={'/reg'}><div className={style.reg}>Sign Up</div></Link>
