e7a6fa953e1ff4b4f33364c257a30bb54aacf394
ArinaBogan <arina.bogan@gmail.com>
2024-01-29 10:23:26 +0300

    added client

--- client/src/pages/CoursesPage/CoursesPage.js
+++ client/src/pages/CoursesPage/CoursesPage.js
@@ -13,30 +13,33 @@ function CoursesPage() {
                     <h1>Courses</h1>
                 </div>
 
-                <div className={style.sectionJS}>
-                    <div className={style.imgJS}></div>
-                    <div className={style.textContentJS}>
-                        <h1>JavaScript</h1>
-                        <p>JavaScript is a practical course where students learn the basics of JavaScript.
-                            It covers variables, operators, conditionals, loops, functions, and data manipulation.</p>
+                <div className={style.allContent}>
+                    <div className={style.sectionJS}>
+                        <div className={style.imgJS}></div>
+                        <div className={style.textContentJS}>
+                            <h1>JavaScript</h1>
+                            <div className={style.line}></div>
+                            <p>JavaScript is a practical course where students learn the basics of JavaScript.
+                                It covers variables, operators, conditionals, loops, functions, and data manipulation.</p>
+                        </div>
                     </div>
-                </div>
 
-                <div className={style.sectionTS}>
-                    <div className={style.imgTS}></div>
-                    <div className={style.textContentTS}>
-                        <h1>TypeScript</h1>
-                        <p>TypeScript is a course that provides an introduction to TypeScript.
-                            Students will learn about TypeScript's key features, such as type annotations, interfaces, classes, and modules</p>
+                    <div className={style.sectionTS}>
+                        <div className={style.imgTS}></div>
+                        <div className={style.textContentTS}>
+                            <h1>TypeScript</h1>
+                            <p>TypeScript is a course that provides an introduction to TypeScript.
+                                Students will learn about TypeScript's key features, such as type annotations, interfaces, classes, and modules</p>
+                        </div>
                     </div>
-                </div>
 
-                <div className={style.sectionPython}>
-                    <div className={style.imgPython}></div>
-                    <div className={style.textContentPython}>
-                        <h1>Python</h1>
-                        <p>Students will learn about variables, data types, conditionals, loops, functions, and file handling.
-                            Through hands-on exercises and projects, students will gain proficiency in writing Python code and solving real-world problems.</p>
+                    <div className={style.sectionPython}>
+                        <div className={style.imgPython}></div>
+                        <div className={style.textContentPython}>
+                            <h1>Python</h1>
+                            <p>Students will learn about variables, data types, conditionals, loops, functions, and file handling.
+                                Through hands-on exercises and projects, students will gain proficiency in writing Python code and solving real-world problems.</p>
+                        </div>
                     </div>
                 </div>
             </div>
