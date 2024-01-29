eec9d6c7b84ea27af4daeac1ac9be43ad823c011
ArinaBogan <arina.bogan@gmail.com>
2024-01-29 14:47:19 +0300

    added client

--- client/src/pages/CoursesPage/CoursesPage.js
+++ client/src/pages/CoursesPage/CoursesPage.js
@@ -13,8 +13,8 @@ function CoursesPage() {
                     <h1>Courses</h1>
                 </div>
 
-                <div className={style.allContent}>
-                    <div className={style.sectionJS}>
+                <div className={style.sectionJS}>
+                    <div className={style.itemsJS}>
                         <div className={style.imgJS}></div>
                         <div className={style.textContentJS}>
                             <h1>JavaScript</h1>
@@ -24,25 +24,33 @@ function CoursesPage() {
                         </div>
                     </div>
 
-                    <div className={style.sectionTS}>
+                </div>
+
+                <div className={style.sectionTS}>
+                    <div className={style.itemsTS}>
                         <div className={style.imgTS}></div>
                         <div className={style.textContentTS}>
                             <h1>TypeScript</h1>
+                            <div className={style.line}></div>
                             <p>TypeScript is a course that provides an introduction to TypeScript.
                                 Students will learn about TypeScript's key features, such as type annotations, interfaces, classes, and modules</p>
                         </div>
                     </div>
+                </div>
 
-                    <div className={style.sectionPython}>
+                <div className={style.sectionPython}>
+                    <div className={style.itemsPython}>
                         <div className={style.imgPython}></div>
                         <div className={style.textContentPython}>
                             <h1>Python</h1>
+                            <div className={style.line}></div>
                             <p>Students will learn about variables, data types, conditionals, loops, functions, and file handling.
                                 Through hands-on exercises and projects, students will gain proficiency in writing Python code and solving real-world problems.</p>
                         </div>
                     </div>
                 </div>
             </div>
+
 
 
             <Footer></Footer>
