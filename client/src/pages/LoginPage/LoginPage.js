e298e0f2c3ac382981cb8492103210980b598bd9
ArinaBogan <arina.bogan@gmail.com>
2024-02-22 18:15:54 +0300

    added education_platform

--- client/src/pages/LoginPage/LoginPage.js
+++ client/src/pages/LoginPage/LoginPage.js
@@ -1,20 +1,31 @@
 import Footer from "../../components/Footer/Footer";
 import Header from "../../components/Header/Header";
+import Input from "../../components/Input/Input";
 import style from './style.module.css';
 
 function LoginPage() {
+
+    const inputArray = [{
+        id: 1,
+        placeholderValue: 'Enter email...',
+        inputType: 'text'
+    }, {
+        id: 2,
+        placeholderValue: 'Enter password...',
+        inputType: 'text'
+    }];
+
     return (
         <>
             <Header></Header>
 
             <div className={style.wrapper}>
-                    <div className={style.textBlock}>
-                        <h1>Login</h1>
-                        <div><input placeholder="Placeholder text"></input></div>
-                        <div><input placeholder="Placeholder text"></input></div>
-                        <div className={style.btn}>Login</div>
-                    </div>
-                    <div className={style.img}></div>
+                <div className={style.textBlock}>
+                    <h1>Login</h1>
+                    <div>{inputArray.map((el) => <Input el={el}></Input>)}</div>
+                    <div className={style.btn}>Login</div>
+                </div>
+                <div className={style.img}></div>
             </div>
             <Footer></Footer>
         </>
